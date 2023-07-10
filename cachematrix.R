c9b16fe4794d29f56323dc2fb8fe64247a1c4aed
elsagambs <139146219+elsagambs@users.noreply.github.com>
2023-07-10 16:42:39 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<- function(x = matrix()) {
+  inv<- NULL
+  set<- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get<- function() {x}
+  setInverse<- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<- function(x, ...) {
+  inv<- x$getInverse()
+  if(!is.null(inv)) {
+    message("gettting chached data")
+    return(inv)
+  }
+  mat<- x$get()
+  inv<- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
+
+# pmatrix<- makeCacheMatrix(matrix(1:16, nrow=4, ncol=4))
+##> pmatrix$get()
+##[,1] [,2] [,3] [,4]
+##[1,]    1    5    9   13
+##[2,]    2    6   10   14
+##[3,]    3    7   11   15
+##[4,]    4    8   12   16
+##> pmatrix$getInverse()
+##NULL
+
+##> pmatrix<- makeCacheMatrix(matrix(1:4, nrow=2, ncol=2))
+##> pmatrix$get()
+##[,1] [,2]
+##[1,]    1    3
+##[2,]    2    4
+##> pmatrix$getInverse()
+##NULL
+##> cacheSolve(pmatrix)
+##[,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
